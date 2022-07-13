c3c7a2a78ba2c7fea9dd3d957467511d20420769
KimMyeongGyun <nam8774@naver.com>
2022-07-13 13:26:08 +0900

    컨트롤러 INFO 추가

--- src/main/java/com/example/logintest/web/UserController.java
+++ src/main/java/com/example/logintest/web/UserController.java
@@ -1,4 +1,11 @@
 package com.example.logintest.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+
 public class UserController {
+    private  final UserService userService;
 }
--- src/main/java/com/example/logintest/web/dto/UserinfoDto.java
+++ src/main/java/com/example/logintest/web/dto/UserinfoDto.java
@@ -1,4 +1,14 @@
 package com.example.logintest.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class UserinfoDto {
+
+    private String email;
+    private String password;
+    private String auth;
 }
