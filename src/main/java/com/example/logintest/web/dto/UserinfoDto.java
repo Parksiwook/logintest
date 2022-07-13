fe1bb874758aa9ff520c3a8d34a6932407a61c31
KimMyeongGyun <nam8774@naver.com>
2022-07-13 15:44:18 +0900

    최종본02

---
+++ src/main/java/com/example/logintest/web/dto/UserinfoDto.java
@@ -0,0 +1,13 @@
+package com.example.logintest.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserinfoDto {
+
+    private String email;
+    private String password;
+    private String auth;
+}
