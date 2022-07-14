246607c6832a19f9e26288f3ff27193ecf54834e
HonamJSH <99239284+HonamJSH@users.noreply.github.com>
2022-07-14 12:07:50 +0900

    UserInfoDto 주석 추가

--- src/main/java/com/example/logintest/Web/dto/UserInfoDto.java
+++ src/main/java/com/example/logintest/Web/dto/UserInfoDto.java
@@ -1,5 +1,6 @@
 package com.example.logintest.Web.dto;
 
+// Lombok = Getter, Setter 자동화
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +8,9 @@ import lombok.Setter;
 @Setter
 public class UserInfoDto {
 
+    // JPA
     private String email;
     private String password;
     private String auth;
 }
 
-// JPA
